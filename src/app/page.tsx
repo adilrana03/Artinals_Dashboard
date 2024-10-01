f61779fa9e9a52add38c5a100d6a73ac1fcaff02
adilrana03 <adilrana2014@gmail.com>
2024-10-01 13:47:12 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function HomePage() {
 	return (
-		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+		<div className='flex flex-col items-center justify-center h-screen '>
 			<main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-				<h1 className='text-6xl font-bold'>
+				<h1 className='md:text-6xl text-5xl font-bold'>
 					Welcome to{" "}
 					<span className='text-transparent bg-clip-text bg-gradient-to-r from-[#6c94f8] to-[#7263fa]'>
 						ARTFI.WORLD
 					</span>
 				</h1>
 
-				<p className='mt-3 text-2xl'>
+				<p className='mt-3 md:text-2xl '>
 					Begin your journey into art investing with us.
 				</p>
 
